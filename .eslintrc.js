57f5c2719a7c2b635f5c75ac9b6be9f53d38f7cd
Evelyne PHICH <evelyne.phich@gmail.com>
2022-01-09 12:52:09 +0100

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "jsx-quotes": ["error", "prefer-double"],
+    quotes: ["error", "double"],
+  },
+};
